8f8dc83061d24c7692583a62081ff8cfcb17b718
Bharanidharan N R <bharaninrb2002@gmail.com>
2024-05-20 14:07:23 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // JavaScript for Email Link
+// © 2024 N.R. Bharanidharan
+// All rights reserved.
+//
+// This code is the proprietary and confidential property of N.R. Bharanidharan.
+// Unauthorized copying of this file, via any medium, is strictly prohibited.
+// Written by N.R. Bharanidharan <bharaninrb2002@gmail.com>, 2024.
 const emailLink = document.querySelector(".email a");
 emailLink.addEventListener("click", function (event) {
   event.preventDefault();
